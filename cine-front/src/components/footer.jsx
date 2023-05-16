02d14894f34107f20a01f7afb939d15cfc882d29
A-Javier-Fuentes <ale.isco.fuentess@gmail.com>
2023-05-16 13:52:30 -0300

    footeryheaderterminados

--- cine-front/src/components/footer.jsx
+++ cine-front/src/components/footer.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 const footer = () => {
   return (
     <div>
       <footer>
         <div className="derechosFooter">
-          
-          <img src="../icons/DH Cinema-logos_black.png" alt="logoCinemaFooter"/>
-          
+            <Link to="../home">
+            <img src="../icons/DH Cinema-logos_black.png" alt="logoCinemaFooter"/>
+            </Link>
             <ul>
                 <li>2023</li>
                 <li>Derechos reservados</li>
             </ul>
         </div>
-        <div className="iconosFooter">
-          <img src="../icons/facebook.png" alt="iconFacebook"/>
-            <img src="../icons/linkedin-in.png" alt="iconLikedIn"/>
-            <img src="../icons/twitter.png" alt="iconTwitter"/>
-            <img src="../icons/instagram.png" alt="iconInstagram"/> 
+            <div className="iconosFooter">
+            <a rel="https://m.facebook.com" target="_blank">
+                <img src="../icons/facebook.png" alt="iconFacebook"/>
+            </a>
+            <a rel="https://www.linkedin.com/login/es" target="_blank">
+                <img src="../icons/linkedin-in.png" alt="iconLikedIn"/>
+            </a>
+            <a rel="https://twitter.com" target="_blank">
+                <img src="../icons/twitter.png" alt="iconTwitter"/>
+            </a>
+            <a rel="https://www.instagram.com/accounts/login/" target="_blank">
+                <img src="../icons/instagram.png" alt="iconInstagram"/>
+            </a>
         </div>
     </footer>
     </div>
--- cine-front/src/components/header.jsx
+++ cine-front/src/components/header.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 const header = () => {
   return (
     <div>
       <header>
         <div className="headerContainer">
+        <Link to="../home">
           <img src="../icons/DH Cinema-logos_black.png" alt="logoCinema"/>
+          </Link>
         </div>
         <div className="headerUsuario">
             <button className="botonCrearCuenta"> Crear Cuenta</button>
