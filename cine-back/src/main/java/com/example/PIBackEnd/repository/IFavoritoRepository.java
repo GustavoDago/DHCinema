29d956b18dc0e4f2cbe2cc7c59fadc245fc3705e
tomigch <tomasgchiappealt@gmail.com>
2023-06-27 18:42:37 -0300

    anadir favoritos terminado

--- cine-back/src/main/java/com/example/PIBackEnd/repository/IFavoritoRepository.java
+++ cine-back/src/main/java/com/example/PIBackEnd/repository/IFavoritoRepository.java
@@ -12,6 +12,6 @@ import java.util.Optional;
 @Repository
 public interface IFavoritoRepository extends JpaRepository<Favorito,Long> {
     boolean existsByUsuarioAndPelicula(Usuario usuario, Pelicula pelicula);
-    List<Favorito> findAllByVigenteTrueAndFavoritoTrueAndUsuario_Email(String email);
+    List<Favorito> findAllByVigenteTrueAndUsuario_Email(String email);
     Optional<Favorito> findByIdAndVigenteTrue(Long id);
 }
--- cine-front/src/components/Nuevo-favorito.jsx
+++ cine-front/src/components/Nuevo-favorito.jsx
@@ -3,22 +3,24 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 const NuevoFavorito = (props) => {
-  const [isFavorited, setIsFavorited] = useState(false);
+  const [isFavorited, setIsFavorited] = useState(null);
   const [favoritoId, setFavoritoId] = useState(null);
+  
 
   useEffect(() => {
     const fetchFavorito = async () => {
       const pelicula_id = props.id;
       const usuario_id = sessionStorage.getItem("id");
+      const email = sessionStorage.getItem("email");
 
       try {
-        const response = await axios.get("http://localhost:8080/favoritos", {
+        const response = await axios.get(`http://localhost:8080/favoritos/${encodeURIComponent(email)}`, {
           params: { pelicula_id, usuario_id },
         });
 
         if (response.data && response.data.id) {
           setFavoritoId(response.data.id);
-          setIsFavorited(true);
+          setIsFavorited(response.data.favorito);
         } else {
           setFavoritoId(null);
           setIsFavorited(false);
@@ -40,27 +42,27 @@ const NuevoFavorito = (props) => {
       usuario_id,
     };
 
-    if (isFavorited) {
+    if (favoritoId) {
       axios
         .put(`http://localhost:8080/favoritos/${favoritoId}`, {
           data: categoryData,
         })
         .then((response) => {
           console.log(response.data); // Handle the server response as needed
-          setIsFavorited(false);
-          alert("Favorito eliminado correctamente");
+          setIsFavorited(!isFavorited);
+          
         })
         .catch((error) => {
           console.error(error); // Handle the error if it occurs
         });
-    } else {
+    }  else {
       axios
         .post("http://localhost:8080/favoritos", categoryData)
         .then((response) => {
           console.log(response.data); // Handle the server response as needed
           setFavoritoId(response.data.id);
           setIsFavorited(true);
-          alert("Favorito añadido correctamente");
+          
         })
         .catch((error) => {
           console.error(error); // Handle the error if it occurs
